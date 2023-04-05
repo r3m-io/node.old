70861eee9cbb4d9ce41a596ee2ceeb82050c27d8
Remco van der Velde <remco.pc@outlook.com>
2023-04-05 14:15:56 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -7,7 +7,7 @@ use R3m\Io\App;
 use R3m\Io\Config;
 use R3m\Io\Exception\ObjectException;
 use R3m\Io\Module\Core;
-use R3m\Io\Module\Dir;
+use R3m\Io\Module\Data as Storage;
 use R3m\Io\Module\File;
 use R3m\Io\Module\Parse;
 use R3m\Io\Module\Sort;
@@ -18,6 +18,8 @@ Trait Data {
 
     public function create($class='', $options=[]): void
     {
+        $data = new Storage($options);
+        ddd($data);
         d($class);
         d($options);
         ddd('end');
