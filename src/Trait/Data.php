9829a53acd1f4d3aad148673b62233ae30cff6c9
Remco van der Velde <remco.pc@outlook.com>
2023-04-05 21:41:40 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -9,11 +9,15 @@ use R3m\Io\Module\Controller;
 use R3m\Io\Module\Core;
 use R3m\Io\Module\Data as Storage;
 use R3m\Io\Module\Dir;
+use R3m\Io\Module\Event;
+use R3m\Io\Module\File;
+use R3m\Io\Module\Validate;
 
 use Exception;
 
 use R3m\Io\Exception\FileWriteException;
 use R3m\Io\Exception\ObjectException;
+
 
 Trait Data {
 
@@ -22,11 +26,11 @@ Trait Data {
      * @throws ObjectException
      * @throws FileWriteException
      */
-    public function create($class='', $options=[]): void
+    public function create($class='', $options=[]): false|array
     {
         $name = Controller::name($class);
         $object = $this->object();
-        $record = new Storage( (object) $options);
+        $node = new Storage( (object) $options);
         $dir_node = $object->config('project.dir.data') .
             'Node' .
             $object->config('ds')
@@ -51,22 +55,110 @@ Trait Data {
                 }
             }
         }
-        $record->set('uuid', Core::uuid());
+        $node->set('uuid', Core::uuid());
         $list = $data->get($class);
         if(empty($list)){
             $list = [];
         }
-        $list[] = $record->data();
-        $data->set($class, $list);
-        $data->write($url);
-        if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
-            $command = 'chmod 666 ' . $url;
-            exec($command);
+        $validate_url =  $dir_class . 'Validate.json';
+        $validate = $this->validate($object, $validate_url,  $class . '.create');
+        $response = [];
+        if($validate) {
+            if($validate->success === true) {
+                $list[] = $node->data();
+                $data->set($class, $list);
+                $data->write($url);
+                if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
+                    $command = 'chmod 666 ' . $url;
+                    exec($command);
+                }
+                if($object->config(Config::POSIX_ID) === 0){
+                    $command = 'chown www-data:www-data ' . $url;
+                    exec($command);
+                }
+                Event::trigger($object, 'r3m.io.node.data.create', [
+                    'class' => $class,
+                    'options' => $options,
+                    'url' => $url,
+                    'node' => $node,
+                ]);
+                $function = 'create';
+                $expose = $this->getExpose(
+                    $object,
+                    $class,
+                    $class . '.' . $function .'.expose'
+                );
+                ddd($expose);
+                $record = $this->expose(
+                    $object,
+                    $node,
+                    $expose,
+                    $class,
+                    $function
+                );
+                $response['node'] = $record;
+            } else {
+                $response['error'] = $validate->test;
+            }
+            return $response;
         }
-        if($object->config(Config::POSIX_ID) === 0){
-            $command = 'chown www-data:www-data ' . $url;
-            exec($command);
-        }
+        return false;
     }
 
+    /**
+     * @throws ObjectException
+     * @throws FileWriteException
+     * @throws Exception
+     */
+    protected function validate(App $object, $url, $type){
+        $data = $object->data(sha1($url));
+        if($data === null){
+            $data = $object->parse_read($url, sha1($url));
+        }
+        if($data){
+            $validate = $data->data($type . '.validate');
+            if(empty($validate)){
+                return false;
+            }
+            return Validate::validate($object, $validate);
+        }
+        return false;
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    protected function expose(App $object, Storage $node, $expose, $class, $function){
+
+        $record = $node->data();
+        return $record;
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    protected function getExpose(App $object, $name='', $attribute=''){
+        $dir_node = $object->config('project.dir.data') .
+            'Node' .
+            $object->config('ds')
+        ;
+        $dir_class = $dir_node .
+            $name .
+            $object->config('ds')
+        ;
+        $url = $dir_class . 'Expose.json';
+        if(!File::exist($url)){
+            throw new Exception('Data url (' . $url . ') not found for class: ' . $name);
+        }
+        $data = $object->data_read($url);
+        if($data){
+            $get = $data->get($attribute);
+            if(empty($get)){
+                throw new Exception('Cannot find attribute (' . $attribute .') in class: ' . $name);
+            }
+            return $get;
+        }
+    }
 }
