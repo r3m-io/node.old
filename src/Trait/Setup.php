cfe1ed013f0442983af1f4eacb8bb644535d5077
Remco van der Velde <remco.pc@outlook.com>
2023-04-07 16:09:23 +0200

    Update Setup.php

--- src/Trait/Setup.php
+++ src/Trait/Setup.php
@@ -29,49 +29,55 @@ Trait Setup {
         if($object->config(Config::POSIX_ID) !== 0){
             return;
         }
-        $source = $object->config('controller.dir.data') .
-            'Node' .
-            $object->config('ds') .
-            'Role' .
-            $object->config('ds') .
-            'Validate.json'
-        ;
-        $dir_node = $object->config('project.dir.data') .
-            'Node' .
-            $object->config('ds')
-        ;
-        $dir_destination = $dir_node .
-            'Role' .
-            $object->config('ds')
-        ;
-        $destination = $dir_destination . 'Validate.json';
-        if(!Dir::is($dir_node)){
-            Dir::create($dir_node, Dir::CHMOD);
-            $command = 'chown www-data:www-data ' . $dir_node;
+        $classes = [
+            'User',
+            'Role',
+        ];
+        foreach ($classes as $class){
+            $source = $object->config('controller.dir.data') .
+                'Node' .
+                $object->config('ds') .
+                $class .
+                $object->config('ds') .
+                'Validate.json'
+            ;
+            $dir_node = $object->config('project.dir.data') .
+                'Node' .
+                $object->config('ds')
+            ;
+            $dir_destination = $dir_node .
+                $class .
+                $object->config('ds')
+            ;
+            $destination = $dir_destination . 'Validate.json';
+            if(!Dir::is($dir_node)){
+                Dir::create($dir_node, Dir::CHMOD);
+                $command = 'chown www-data:www-data ' . $dir_node;
+                exec($command);
+                if($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
+                    $command = 'chmod 777 ' . $dir_node;
+                    exec($command);
+                }
+            }
+            if(!Dir::is($dir_destination)){
+                Dir::create($dir_destination, Dir::CHMOD);
+                $command = 'chown www-data:www-data ' . $dir_destination;
+                exec($command);
+                if($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
+                    $command = 'chmod 777 ' . $dir_destination;
+                    exec($command);
+                }
+            }
+            if(File::exist($destination)){
+                File::delete($destination);
+            }
+            File::copy($source, $destination);
+            $command = 'chown www-data:www-data ' . $destination;
             exec($command);
-            if($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
-                $command = 'chmod 777 ' . $dir_node;
+            if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
+                $command = 'chmod 666 ' . $destination;
                 exec($command);
             }
-        }
-        if(!Dir::is($dir_destination)){
-            Dir::create($dir_destination, Dir::CHMOD);
-            $command = 'chown www-data:www-data ' . $dir_destination;
-            exec($command);
-            if($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
-                $command = 'chmod 777 ' . $dir_destination;
-                exec($command);
-            }
-        }
-        if(File::exist($destination)){
-            File::delete($destination);
-        }
-        File::copy($source, $destination);
-        $command = 'chown www-data:www-data ' . $destination;
-        exec($command);
-        if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
-            $command = 'chmod 666 ' . $destination;
-            exec($command);
         }
     }
 }
