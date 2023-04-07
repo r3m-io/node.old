f5abb1fdf2f884cbc62fbcb53c77de7a14649a47
Remco van der Velde <remco.pc@outlook.com>
2023-04-07 16:58:59 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -129,6 +129,49 @@ Trait Data {
         return null;
     }
 
+    public function delete($class, $options=[]): bool
+    {
+        $name = Controller::name($class);
+        $object = $this->object();
+        $node = new Storage( (object) $options);
+        $dir_node = $object->config('project.dir.data') .
+            'Node' .
+            $object->config('ds')
+        ;
+        $dir_class = $dir_node .
+            $name .
+            $object->config('ds')
+        ;
+        $url = $dir_class . 'Data.json';
+        $data = $object->data_read($url);
+        if(!$data){
+            return false;
+        }
+        $list = $data->get($class);
+        if(empty($list)){
+            $list = [];
+        }
+        $uuid = $node->get('uuid');
+        foreach($list as $nr => $record){
+            if(
+                is_array($record) &&
+                array_key_exists('uuid', $record) &&
+                $record['uuid'] === $uuid
+            ){
+                unset($list[$nr]);
+                break;
+            }
+        }
+        $result = [];
+        foreach($list as $record){
+            $result[] = $record;
+        }
+        $data->set($class, $result);
+        $data->write($url);
+        return true;
+    }
+
+
     /**
      * @throws ObjectException
      */
