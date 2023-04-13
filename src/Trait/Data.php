47e92bf61bd9d355feb8b740ed8e6d5b30599710
Remco van der Velde <remco.pc@outlook.com>
2023-04-13 17:09:12 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -457,8 +457,7 @@ Trait Data {
                 }
             }
         }
-        ddd($meta);
-        foreach($meta->get('BinarySeach.' . $class)  as $property => $record){
+        foreach($meta->get('BinarySearch.' . $class)  as $property => $record){
             if($property === 'Uuid'){
                 continue;
             }
