1c414f6b3a9f95a5985144aa1a3f3d49e9c739f3
Remco van der Velde <remco.pc@outlook.com>
2023-04-15 01:24:35 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -912,7 +912,11 @@ Trait Data {
                             $object_match = str_replace('"', '', $object_match);
                             $object_explode = explode(':', $object_match);
                             $symbol = trim($object_explode[0], " \t\n\r\0\x0B,");
-                            $symbol_right = trim($object_explode[1], " \t\n\r\0\x0B,");
+                            $symbol_right = null;
+                            if(array_key_exists(1, $object_explode)){
+                                $symbol_right = trim($object_explode[1], " \t\n\r\0\x0B,");
+                            }
+
                             if(
                                 $symbol === '}' ||
                                 $symbol_right === '}'
