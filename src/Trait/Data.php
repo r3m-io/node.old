21bdd898b028f9fa2979adb16da9db2be559c1d9
Remco van der Velde <remco.pc@outlook.com>
2023-04-15 03:26:11 +0200

    Update Data.php

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -998,7 +998,7 @@ Trait Data {
                     ){
                         $options['seek'] = (int) (1.5 * $options['seek']);
                         if($options['seek'] > $options['lines']){
-                            $options['seek'] = $options['lines'];
+                            $options['seek'] = $options['lines'] - 1;
                         }
                         return $this->bin_search_index($file, $options);
                     }
