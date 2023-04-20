671a76898a40d109159f40f8fffca0a63343a22a
remco van der Velde <remco.pc@outlook.com>
2023-04-20 18:53:33 +0200

    create where list

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -1318,6 +1318,7 @@ Trait Data {
             while($set = $this->filter_where_process($record, $set, $where, $key)){
                 $counter++;
                 if(count($set) === 1 && $set[0] === true){
+                    d($record);
                     return $record;
                 }
                 if(empty($set)){
