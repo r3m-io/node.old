e84224dbb0412420fb7f3bb5511f6a718327ebaa
remco van der Velde <remco.pc@outlook.com>
2023-04-21 15:28:02 +0200

    create where list

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -1431,6 +1431,12 @@ Trait Data {
                 break;
             }
             $set = $this->filter_where_get_set($where, $key, $deepest);
+            if(array_key_exists('debug', $options)){
+                d($where);
+                d($key);
+                d($deepest);
+                ddd($set);
+            }
             while($record !== false){
                 $set = $this->where_process($record, $set, $where, $key, $operator);
                 if(empty($set) && $deepest === 0){
