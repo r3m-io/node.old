bd9143acf2c3c8ba73e4bbea4371e6b8dc613e14
remco van der Velde <remco.pc@outlook.com>
2023-04-21 15:53:26 +0200

    this is nice

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -1497,6 +1497,7 @@ Trait Data {
                 }
                 $count_set = count($set);
                 if($count_set === 1){
+                    d($set);
                     if($operator === null && $set[0] === true){
                         break;
                     } else {
