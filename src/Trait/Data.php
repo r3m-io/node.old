b87d24a0ee51203f1320815882d965b675618662
remco van der Velde <remco.pc@outlook.com>
2023-04-24 20:54:58 +0200

    node data

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -970,13 +970,13 @@ Trait Data {
 {
         $attribute = '';
         if(!array_key_exists('collection', $record)){
-            return $record['execute'] ? $record['execute'] : $record['value'];
+            return array_key_exists('execute', $record) ? $record['execute'] : $record['value'];
         }
         if(!is_array($record['collection'])){
-            return $record['execute'] ? $record['execute'] : $record['value'];
+            return array_key_exists('execute', $record) ? $record['execute'] : $record['value'];
         }
         foreach($record['collection'] as $nr => $record){
-            $attribute .= $record['execute'] ? $record['execute'] : $record['value'];
+            $attribute .= array_key_exists('execute', $record) ? $record['execute'] : $record['value'];
         }
         return $attribute;
     }
