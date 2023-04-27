7a2d4e6097edd2de4adf86d1693d468a0ae3a3db
remco van der Velde <remco.pc@outlook.com>
2023-04-27 22:47:08 +0200

    node data

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -556,6 +556,7 @@ Trait Data {
                         ]);
                     } else {
                         $sort_key = sha1(Core::object($properties, Core::OBJECT_JSON));
+                        d($sort_key);
                         $lines = $meta->get('Sort.' . $class . '.' . $sort_key . '.lines');
                         ddd($lines);
                         $file = new SplFileObject($url);
