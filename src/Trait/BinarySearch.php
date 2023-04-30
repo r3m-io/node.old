a3436295f64184089cba2dfdd2107d1c81f35601
remco van der Velde <remco.pc@outlook.com>
2023-04-30 03:12:55 +0200

    node data;

--- src/Trait/BinarySearch.php
+++ src/Trait/BinarySearch.php
@@ -263,7 +263,6 @@ Trait BinarySearch {
                     $record = $this->filter($record, $options['filter'], $options);
                 }
                 elseif(!empty($options['where'])){
-                    ddd($options['where']);
                     $record = $this->where($record, $options['where'], $options);
                 }
                 if($record){
@@ -323,6 +322,7 @@ Trait BinarySearch {
                     $record = $this->filter($record, $options['filter'], $options);
                 }
                 elseif(!empty($options['where'])){
+                    ddd($options['where']);
                     $record = $this->where($record, $options['where'], $options);
                 }
                 if($record){
