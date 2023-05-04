75795af8b02a6525dae61ecd136a29aa13be493b
remco van der Velde <remco.pc@outlook.com>
2023-05-04 17:14:56 +0200

    node data;

---
+++ src/Validator/Is.Unique.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2020-09-18
+ * @copyright       Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ * @changeLog
+ *     -            all
+ */
+use R3m\Io\App;
+
+function validate_is_unique(App $object, $options=[])
+{
+    ddd($options);
+
+}
