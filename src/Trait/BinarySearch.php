7d0e3d3cb52a54886139a68e5fbaf4ee0bd8447e
remco van der Velde <remco.pc@outlook.com>
2023-05-05 19:26:21 +0200

    node data

--- src/Trait/BinarySearch.php
+++ src/Trait/BinarySearch.php
@@ -265,6 +265,8 @@ Trait BinarySearch {
                     $record = $this->filter($record, $options['filter'], $options);
                 }
                 elseif(!empty($options['where'])){
+                    $debug = debug_backtrace(true);
+                    ddd($debug);
                     d($options['where']);
                     ddd($record);
                     $record = $this->where($record, $options['where'], $options);
