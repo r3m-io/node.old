8be7d88bdbff0a7d66dcf8702f7d1af4b7bc3d25
remco van der Velde <remco.pc@outlook.com>
2023-05-06 14:57:42 +0200

    node data

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -2,15 +2,28 @@
 
 namespace R3m\Io\Node\Trait\Data;
 
+use R3m\Io\Module\File;
 use R3m\Io\Module\Controller;
 use R3m\Io\Module\Data as Storage;
 
 use R3m\Io\Exception\FileWriteException;
 use R3m\Io\Exception\ObjectException;
 
+
 Trait Import {
 
     public function import($class, $options=[]){
+        if(!array_key_exists('url', $options)){
+            return;
+        }
+        if(!File::exist($options['url'])){
+            return;
+        }
+        $object = $this->object();
+        $data = $object->data_read($options['url']);
+
+        ddd($data);
+
         d($class);
         ddd($options);
     }
