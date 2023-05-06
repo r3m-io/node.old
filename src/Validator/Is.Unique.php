052b57fa5b4f4c168a1e67f328a73e9cb6dc91b5
remco van der Velde <remco.pc@outlook.com>
2023-05-07 01:31:00 +0200

    node data

--- src/Validator/Is.Unique.php
+++ src/Validator/Is.Unique.php
@@ -17,20 +17,73 @@ use R3m\Io\Module\File;
 function validate_is_unique(App $object, $value='', $attribute='', $validate='')
 {
     $url = false;
+    $class = false;
     if(is_object($validate)){
         if(property_exists($validate, 'url')){
             $url = $validate->url;
         }
+        if(property_exists($validate, 'class')){
+            $class = $validate->class;
+        }
+        if(property_exists($validate, 'attribute')){
+            $attribute = $validate->attribute;
+        }
+        if(property_exists($validate, 'value')){
+            $value = $validate->value;
+            if(is_array($value)){
+                foreach($value as $nr => $record){
+                    $value[$nr] = $object->get($record);
+                }
+            }
+        }
     }
+    if(
+        is_array($attribute) &&
+        is_array($value)
+    ){
+        $options = [
+            'filter' => [],
+            'sort' => []
+        ];
+        d($attribute);
+        d($value);
+        ddd($object);
+
+        foreach($attribute as $nr => $record){
+            if(array_key_exists($nr, $value)){
+                $options['filter'][$record] = $value[$nr];
+                $options['sort'][$record] = 'ASC';
+            }
+        }
+    } else {
+        $options = [
+            'filter' => [
+                $attribute => $value
+            ],
+            'sort' => [
+                $attribute => 'ASC'
+            ]
+        ];
+    }
+    /*
     if($url === false){
         throw new Exception('Url not set for Is.Unique');
     }
     if(File::exist($url) === false){
         throw new Exception('BinarySearch tree not found for Is.Unique (' . $url .')');
     }
-    d($url);
+    */
 
+    $unique = new Unique();
+    $record = $unique->record($class, $options);
+    d($url);
+    d($record);
     d($value);
     ddd($validate);
+
+}
+
+class Unique {
+    use R3m\Io\Node\Trait\Data;
 
 }
