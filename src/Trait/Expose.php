70deb109d50d96ef6e072eaabe9ec2c221632570
remco van der Velde <remco.pc@outlook.com>
2023-05-08 15:46:07 +0200

    expose

--- src/Trait/Expose.php
+++ src/Trait/Expose.php
@@ -92,6 +92,10 @@ Trait Expose {
                                 $action->role === $parentRole
                             )
                         ) {
+                            if($function === 'child'){
+                                d($action);
+                                ddd($expose);
+                            }
                             if (
                                 property_exists($action, 'attributes') &&
                                 is_array($action->attributes)
