a32b82b9e05b3bfe639d49920a05f895318f8a65
remco van der Velde <remco.pc@outlook.com>
2023-05-11 21:06:21 +0200

    expose

--- src/Validator/Is.Unique.php
+++ src/Validator/Is.Unique.php
@@ -80,7 +80,8 @@ function validate_is_unique(App $object, $value='', $attribute='', $validate='')
         $unique = new Unique($parse, $data);
         $object->data('Is.Unique', $unique);
     }
-    $record = $unique->record($class, $options);
+
+    $record = $unique->record($class, $unique->role_system(), $options);
     if (empty($record)) {
         return true;
     }
