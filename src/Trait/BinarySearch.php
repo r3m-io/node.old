3ace17f0e27c6838f3f7acf0a5a6a3627f3f9e6f
remco van der Velde <remco.pc@outlook.com>
2023-05-22 15:17:15 +0200

    expose

--- src/Trait/BinarySearch.php
+++ src/Trait/BinarySearch.php
@@ -631,7 +631,7 @@ Trait BinarySearch {
         }
         if(!array_key_exists('function', $options)){
             $debug = debug_backtrace(true);
-            d($debug[1]['function'] . ' is missing function in options');
+            d($debug);
             ddd($debug[1]['file'] . ' ' . $debug[1]['line']);
         }
         $start = $index;
