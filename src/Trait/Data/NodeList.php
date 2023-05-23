cac6caeba7203ddd3b6d337a18bd58107e59cc36
remco van der Velde <remco.pc@outlook.com>
2023-05-23 15:50:18 +0200

    expose

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -31,6 +31,7 @@ Trait NodeList {
         if(!array_key_exists('relation', $options)){
             $options['relation'] = true;
         }
+        d($options);
         $object = $this->object();
         if(!array_key_exists('sort', $options)){
             throw new Exception('Sort is missing in options for ' . $name . '::' . $options['function'] . '()');
