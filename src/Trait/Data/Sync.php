19c531d57954a3026b891301cf2f7d94c7028baa
remco van der Velde <remco.pc@outlook.com>
2023-05-23 12:57:41 +0200

    expose

--- src/Trait/Data/Sync.php
+++ src/Trait/Data/Sync.php
@@ -174,6 +174,7 @@ Trait Sync {
                         $url_property_desc = false;
                         $mtime_property = File::mtime($url_property_asc);
                     }
+                    $mtime_property = false;
                     if ($mtime === $mtime_property) {
                         //same cache
                         continue;
