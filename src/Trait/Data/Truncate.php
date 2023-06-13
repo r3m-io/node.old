f8413fe1d566e2995116d147ea1e504fa67bae63
remco van der Velde <remco.pc@outlook.com>
2023-06-13 18:03:40 +0200

    truncate

--- src/Trait/Data/Truncate.php
+++ src/Trait/Data/Truncate.php
@@ -58,11 +58,11 @@ Trait Truncate {
         $count = $meta->get('Sort.' . $name . '.' . $sort_key . '.' . 'count');
 //        $url_property = $meta->get('Sort.' . $name . '.' . $sort_key . '.' . $url_key);
         $page_max = ceil($count / $list_options['limit']);
-        ddd($page_max);
         for($page=1; $page <= $page_max; $page++) {
             $list_options['page'] = $page;
             $response = $this->list($name, $role, $list_options);
             $list = [];
+            ddd($response);
             foreach ($response['list'] as $record) {
                 if(
                     is_array($record) &&
