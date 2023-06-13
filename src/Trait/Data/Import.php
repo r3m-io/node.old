1f379fc68ab786459310f73db4aaba4f4d2ceaa5
remco van der Velde <remco.pc@outlook.com>
2023-06-13 15:15:08 +0200

    export

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -2,14 +2,14 @@
 
 namespace R3m\Io\Node\Trait\Data;
 
-use R3m\Io\Module\Dir;
-use R3m\Io\Module\File;
 use R3m\Io\Module\Controller;
 use R3m\Io\Module\Data as Storage;
+use R3m\Io\Module\Dir;
+use R3m\Io\Module\File;
+use R3m\Io\Module\Sort;
 
 use R3m\Io\Exception\FileWriteException;
 use R3m\Io\Exception\ObjectException;
-use R3m\Io\Module\Sort;
 
 
 Trait Import {
