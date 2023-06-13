a6de034c1c960027e2ea24bfe0cf25a7d71e1fa8
remco van der Velde <remco.pc@outlook.com>
2023-06-13 19:10:55 +0200

    bugfix

--- src/Trait/BinarySearch.php
+++ src/Trait/BinarySearch.php
@@ -1054,6 +1054,7 @@ Trait BinarySearch {
             $data[$nr] = ltrim($line);
         }
         $data = implode('', $data);
+        d($data);
         $record  = Core::object($data, Core::OBJECT_OBJECT);
         if(!is_object($record)){
             return false;
