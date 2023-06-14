d9eec60bb6221019ece2f64cf83e433583d75901
remco van der Velde <remco.pc@outlook.com>
2023-06-15 01:07:00 +0200

    put many

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -34,6 +34,7 @@ Trait Create {
                 'function' => $options['function'] ?? __FUNCTION__,
                 ]
             );
+            d($record);
             ddd($create);
             $result[] = $create;
         }
