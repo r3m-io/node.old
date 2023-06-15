1a864230ae3b274f68bb8f3720e4829213a3bed5
remco van der Velde <remco.pc@outlook.com>
2023-06-15 20:38:53 +0200

    import utf-8 charlist

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -303,7 +303,6 @@ Trait Create {
                         __FUNCTION__,
                         $role
                     );
-                    ddd($record);
                     if (
                         $record->has('uuid') &&
                         !empty($record->get('uuid'))
