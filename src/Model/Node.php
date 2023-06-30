214a26140944986e51637af0673100d57d7dccd4
remco van der Velde <remco.pc@outlook.com>
2023-06-30 20:45:31 +0200

    node model

--- src/Model/Node.php
+++ src/Model/Node.php
@@ -1,13 +1,16 @@
 <?php
 namespace R3m\Io\Node\Model;
 
+use R3m\Io\Module\Data as Storage;
 use R3m\Io\Module\Template\Main;
+
 use R3m\Io\Node\Trait\Data;
 
 class Node extends Main {
     use Data;
 
-    public function __construct(){
-
+    public function __construct(App $object){
+        $this->object($object);
+        $this->storage(new Storage());
     }
 }
