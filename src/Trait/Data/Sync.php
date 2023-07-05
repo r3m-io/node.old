6695d39fd77dd08231fdb3146ed912e0ba19f256
remco van der Velde <remco.pc@outlook.com>
2023-07-05 16:02:52 +0200

    BinaryTree

--- src/Trait/Data/Sync.php
+++ src/Trait/Data/Sync.php
@@ -324,8 +324,8 @@ Trait Sync {
                                 }
                             }
                         }
-//                        d($connect_property_uuid);
-//                        d($connect_uuid_property);
+                        d($connect_property_uuid);
+                        d($connect_uuid_property);
                         Dir::create($dir_property_asc_asc, Dir::CHMOD);
                         $connect_asc_asc_lines = File::write($url_connect_asc_asc, implode(PHP_EOL, $connect_property_uuid), 'lines');
                         File::touch($url_connect_asc_asc, $mtime);
@@ -369,8 +369,6 @@ Trait Sync {
                                 }
                             }
                         }
-                        d($connect_property_uuid);
-                        d($connect_uuid_property);
                         Dir::create($dir_property_asc_desc, Dir::CHMOD);
                         $connect_asc_desc_lines = File::write($url_connect_asc_desc, implode(PHP_EOL, $connect_property_uuid), 'lines');
                         File::touch($url_connect_asc_desc, $mtime);
