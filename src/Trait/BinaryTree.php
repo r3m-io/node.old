bf70ab1f031405603589ea737fb0f4d49304ae56
remco van der Velde <remco.pc@outlook.com>
2023-07-10 01:15:39 +0200

    ;

--- src/Trait/BinaryTree.php
+++ src/Trait/BinaryTree.php
@@ -954,6 +954,7 @@ Trait BinaryTree {
                 break;
             }
         }
+        d($page);
         if(
             array_key_exists('mtime', $options) &&
             array_key_exists('ramdisk', $options) &&
