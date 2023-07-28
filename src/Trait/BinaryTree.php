f5a41ba3064106955e13bbc5050eeb0f8f41633d
remco van der Velde <remco.pc@outlook.com>
2023-07-28 19:00:24 +0200

    r3m-io/node rename

--- src/Trait/BinaryTree.php
+++ src/Trait/BinaryTree.php
@@ -893,6 +893,10 @@ Trait BinaryTree {
                 $record
             ){
                 $read = $object->data_read($record->{'#read'}->url, sha1($record->{'#read'}->url));
+                if($read && $read->has('#class') && $read->get('#class') === 'App.Event'){
+                    $read->set('#class', 'Server.Event');
+                    $read->write($record->{'#read'}->url);
+                }
                 if($read){
                     $record = Core::object_merge($record, $read->data());
                 }
