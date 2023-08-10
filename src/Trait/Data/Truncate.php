4334e240e87491fae4848a3de06a16a836f27829
remco van der Velde <remco.pc@outlook.com>
2023-08-10 19:25:01 +0200

    truncate

--- src/Trait/Data/Truncate.php
+++ src/Trait/Data/Truncate.php
@@ -30,7 +30,6 @@ Trait Truncate {
             $object->config('extension.json')
         ;
         $meta = $object->data_read($meta_url);
-        ddd($meta);
         $list_options = [
             'sort' => [
                 'uuid' => 'asc'
