2cda0076e3567b60c54b0e4e87cc2e7913617b34
remco van der Velde <remco.pc@outlook.com>
2023-08-11 21:25:54 +0200

    import

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -244,6 +244,7 @@ Trait Create {
         $sort = new Sort();
         usort($list, array($sort,"uuid_compare_ascending"));
         Dir::create($dir_binary_tree_asc, Dir::CHMOD);
+        ddd(implode(PHP_EOL, $list));
         $lines = File::write($binary_tree_url, implode(PHP_EOL, $list), File::LINES);
         $mtime = File::mtime($binary_tree_url);
         $this->sync_file([
@@ -421,7 +422,6 @@ Trait Create {
         $sort = new Sort();
         usort($list, array($sort,"uuid_compare_ascending"));
         Dir::create($dir_binary_tree_asc, Dir::CHMOD);
-        ddd(implode(PHP_EOL, $list));
         $lines = File::write($binary_tree_url, implode(PHP_EOL, $list), File::LINES);
         $mtime = File::mtime($binary_tree_url);
         $list = Sort::list($list)->with([
