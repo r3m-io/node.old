8112036cd930b2a61bec38ab17607c5fb0d4b42b
remco van der Velde <remco.pc@outlook.com>
2023-08-11 22:27:56 +0200

    import

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -241,8 +241,6 @@ Trait Create {
                 }
             }
         }
-        Core::interactive();
-        d($list);
         $sort = new Sort();
         usort($list, array($sort,"uuid_compare_ascending"));
         Dir::create($dir_binary_tree_asc, Dir::CHMOD);
