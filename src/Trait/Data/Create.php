ff56eb3b6ab99ab70b9d8e134a3e6cc95e9f4b8d
remco van der Velde <remco.pc@outlook.com>
2023-08-11 19:44:51 +0200

    import

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -216,6 +216,7 @@ Trait Create {
         }
         $sort = new Sort();
         usort($list, array($sort,"uuid_compare_ascending"));
+        ddd($list);
         Dir::create($dir_binary_tree_asc, Dir::CHMOD);
         $lines = File::write($binary_tree_url, implode(PHP_EOL, $list), File::LINES);
         $mtime = File::mtime($binary_tree_url);
