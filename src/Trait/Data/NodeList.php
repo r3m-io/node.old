0b7f661db969626f0abae098ea003403063bfabe
remco van der Velde <remco.pc@outlook.com>
2023-08-12 14:28:42 +0200

    where

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -147,7 +147,6 @@ Trait NodeList {
                 $object->config('extension.json')
             ;
             $ramdisk_data = $object->data_read($ramdisk_url, $ramdisk_key);
-            d($ramdisk_data);
             if($ramdisk_data){
                 //add mtime to ramdisk data
                 return (array) $ramdisk_data->data();
