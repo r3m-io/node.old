584e130f3f64c5c34032314c0ab2bf97e26962bd
remco van der Velde <remco.pc@outlook.com>
2023-08-13 22:12:05 +0200

    filter

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -69,7 +69,7 @@ Trait NodeList {
         if(!array_key_exists('sort', $options)){
             throw new Exception('Sort is missing in options for ' . $name . '::' . $options['function'] . '()');
         }
-        d($options);
+//        d($options);
         $first = true;
         $properties = [];
         $url_connect_key = '';
