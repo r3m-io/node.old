de2bc7aa27b12f3b9b022fad87b9e0d7bc0a2225
remco van der Velde <remco.pc@outlook.com>
2023-08-18 02:29:54 +0200

    node create

--- src/Trait/BinaryTree.php
+++ src/Trait/BinaryTree.php
@@ -1316,9 +1316,9 @@ Trait BinaryTree {
                 }
             }
             $file->seek($seek);
-            echo 'Seek: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
+            echo 'Seek 1: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
             $line = $file->current();
-//            echo $line . PHP_EOL;
+            echo $line . PHP_EOL;
             $options['counter']++;
             if($options['counter'] > 1024){
                 throw new Exception('Out of range');
@@ -1327,9 +1327,9 @@ Trait BinaryTree {
             }
             if ($options['index'] === $seek) {
                 $options['line'] = $line;
-                echo 'Seek: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
+                echo 'Seek 2: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
                 $uuid = $this->binary_tree_uuid($file, $file_uuid, $file_connect_property, $options);
-//                echo 'UUID: ' . $uuid . PHP_EOL;
+                echo 'UUID: ' . $uuid . PHP_EOL;
                 if($uuid){
                     $record = [];
                     $record['uuid'] = $uuid;
