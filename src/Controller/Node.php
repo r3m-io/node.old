905378e31be1c42c14756234d4a6d9cd9d65dc31
remco van der Velde <remco.pc@outlook.com>
2023-08-22 20:40:10 +0200

    controller node

---
+++ src/Controller/Node.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace R3m\Io\Node\Controller;
+
+use R3m\Io\App;
+
+
+use R3m\Io\Module\Controller;
+use R3m\Io\Module\Response;
+use R3m\Io\Node\Model\Node as Model;
+
+use Exception;
+
+use R3m\Io\Exception\ObjectException;
+use R3m\Io\Exception\FileWriteException;
+
+class Node extends Controller {
+    const DIR = __DIR__ . '/';
+
+    /**
+     * @throws ObjectException
+     * @throws FileWriteException
+     * @throws Exception
+     */
+    public static function list(App $object): Response
+    {
+        $model = new Model($object);
+
+        $sort = $object->request('sort');
+        if(empty($sort)){
+            $sort = [
+                'uuid' => 'ASC'
+            ];
+        }
+        $filter = $object->request('filter');
+        if(empty($filter)){
+            $filter = [];
+        }
+        elseif(!is_array($filter)){
+            throw new Exception('Filter must be an array.');
+        }
+        $where = $object->request('where');
+        if(empty($where)){
+            $where = [];
+        }
+        $response = $model->list(
+            $object->request('class'),
+            $model->role_system(), //leak
+            [
+                'sort' => $sort,
+                'filter' => $filter,
+                'where' => $where,
+                'limit' => (int) $object->request('limit'),
+                'page' => (int) $object->request('page')
+            ]
+        );
+        return new Response(
+            $response,
+            Response::TYPE_JSON
+        );
+    }
+
+}
