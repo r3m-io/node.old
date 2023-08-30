954cbb3afbbb35039e6d9f1d40a98490cd283314
remco van der Velde <remco.pc@outlook.com>
2023-08-30 20:57:55 +0200

    improving patch

--- src/Trait/Data/Record.php
+++ src/Trait/Data/Record.php
@@ -24,7 +24,10 @@ Trait Record {
         if(!array_key_exists('sort', $options)){
             throw new Exception('Sort is missing in options');
         }
+        d($class);
+        d($options);
         $list = $this->list($class, $role, $options);
+        ddd($list);
         if(
             is_array($list) &&
             array_key_exists('list', $list) &&
