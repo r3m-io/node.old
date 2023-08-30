150058dd79866725fc5cc8ea7c8b1c2e911cc003
remco van der Velde <remco.pc@outlook.com>
2023-08-30 23:09:09 +0200

    relation not working in list

--- src/Trait/BinaryTree.php
+++ src/Trait/BinaryTree.php
@@ -898,6 +898,7 @@ Trait BinaryTree {
                     $options['name'] ?? ucfirst($record->{'#class'}) .
                     $object->config('extension.json')
                 ;
+                d($object_url);
                 $options_json = Core::object($options, Core::OBJECT_JSON);
                 $object_data = $object->data_read($object_url, sha1($object_url . '.' . $options_json));
                 /*
