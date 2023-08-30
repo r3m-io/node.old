1f850da75906b8d1d0a3aa009e037d5992ae5bb7
remco van der Velde <remco.pc@outlook.com>
2023-08-30 23:04:11 +0200

    improving patch

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -510,6 +510,7 @@ Trait NodeList {
                             'mtime' => $mtime
                         ]
                     );
+                    ddd($list);
                     $result = [];
                     $result['page'] = $options['page'] ?? 1;
                     $result['limit'] = $options['limit'] ?? 1000;
