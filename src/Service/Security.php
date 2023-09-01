4cbb0694a4d531e10d057aecfb88c59476a9c78a
remco van der Velde <remco.pc@outlook.com>
2023-09-01 21:50:39 +0200

    security

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -1,36 +1,20 @@
 <?php
 namespace R3m\Io\Node\Service;
 
-use Entity\Role;
-use Entity\User as Entity;
+use R3m\Io\App;
+use R3m\Io\Module\Controller;
+use R3m\Io\Module\Data;
 
 use Exception;
-
-use R3m\Io\App;
-use R3m\Io\Module\Core;
-use R3m\Io\Module\Data;
-use R3m\Io\Module\Database;
-use R3m\Io\Module\Dir;
-use R3m\Io\Module\File;
-use R3m\Io\Module\Handler;
-use R3m\Io\Module\Parse;
-
-use R3m\Io\Exception\ErrorException;
-use R3m\Io\Exception\AuthorizationException;
-use R3m\Io\Exception\ObjectException;
-use R3m\Io\Exception\FileWriteException;
-
-use Doctrine\ORM\Exception\ORMException;
-use stdClass;
 
 class Security extends Main
 {
 
     public static function is_granted($class, $role, $options){
+        $name = Controller::name($class);
+        $role = new Data($role);
         d($class);
-        d($role);
-        ddd($options);
+        d($options);
+        ddd($role);
     }
-
-
 }
