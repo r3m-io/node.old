9dc03f8546377756cc276a13b726b20de788a35b
remco van der Velde <remco.pc@outlook.com>
2023-09-01 16:10:43 +0200

    parse

--- src/Trait/Filter.php
+++ src/Trait/Filter.php
@@ -6,12 +6,12 @@ use R3m\Io\Module\Controller;
 use R3m\Io\Module\Data as Storage;
 use R3m\Io\Module\File;
 use R3m\Io\Module\Filter as Module;
+use R3m\Io\Module\Parse;
 
 use Exception;
 
 use R3m\Io\Exception\FileWriteException;
 use R3m\Io\Exception\ObjectException;
-use R3m\Io\Module\Parse;
 
 Trait Filter {
 
