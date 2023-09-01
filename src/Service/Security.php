4d7772bbf9a3093c00243aed198b6007a7f426a5
remco van der Velde <remco.pc@outlook.com>
2023-09-01 22:11:52 +0200

    security

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -13,21 +13,48 @@ class Security extends Main
     /**
      * @throws Exception
      */
-    public static function is_granted($class, $role, $options){
+    public static function is_granted($class, $role, $options): bool
+    {
         if(!array_key_exists('function', $options)){
             throw new Exception('Function is missing in options');
         }
         $name = Controller::name($class);
         $role = new Data($role);
-
-        d($options);
-        d($name . '.' . $options['function']);
-
+        $is_permission = false;
+        $is_permission_relation = false;
+        $is_permission_parse = false;
+        $permissions = [
+            $name . '.' . $options['function'],
+            $name . '.' . $options['function'] . '.' . 'relation',
+            $name . '.' . $options['function'] . '.' . 'parse',
+        ];
         foreach($role->get('permission') as $permission){
             $permission = new Data($permission);
             if($permission->get('name') === $name . '.' . $options['function']){
+                $is_permission = true;
+            }
+            if($options['relation'] === true){
+                if($permission->get('name') === $name . '.' . $options['function'] . '.' . 'relation'){
+                    $is_permission_relation = true;
+                }
+            } else {
+                $is_permission_relation = true;
+            }
+            if($options['parse'] === true) {
+                if($permission->get('name') === $name . '.' . $options['function'] . '.' . 'parse'){
+                    $is_permission_parse = true;
+                }
+            } else {
+                $is_permission_parse = true;
+            }
+            if(
+                $is_permission === true &&
+                $is_permission_parse === true &&
+                $is_permission_relation === true
+            ){
                 return true;
             }
         }
+        throw new Exception('Security: permission denied... (' . implode(', ', $permissions) . ')');
     }
 }
