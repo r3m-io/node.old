ad2208932cd3a49e229e00a29f1e1ee1fd5f4d64
remco van der Velde <remco.pc@outlook.com>
2023-09-01 22:25:47 +0200

    security

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -25,6 +25,18 @@ class Security extends Main
         $is_permission_parse = false;
         $permissions = [];
         $permissions[] = $name . '.' . $options['function'];
+        if(
+            array_key_exists('relation', $options) &&
+            $options['relation'] === true
+        ){
+            $permissions[] = $name . '.' . $options['function'] . '.' . 'relation';
+        }
+        if(
+            array_key_exists('parse', $options) &&
+            $options['parse'] === true
+        ){
+            $permissions[] = $name . '.' . $options['function'] . '.' . 'parse';
+        }
         foreach($role->get('permission') as $permission){
             $permission = new Data($permission);
             if($permission->get('name') === $name . '.' . $options['function']){
@@ -34,10 +46,8 @@ class Security extends Main
                 array_key_exists('relation', $options) &&
                 $options['relation'] === true
             ){
-                $permissions[] = $name . '.' . $options['function'] . '.' . 'relation';
                 if($permission->get('name') === $name . '.' . $options['function'] . '.' . 'relation'){
                     $is_permission_relation = true;
-
                 }
             } else {
                 $is_permission_relation = true;
@@ -46,7 +56,6 @@ class Security extends Main
                 array_key_exists('parse', $options) &&
                 $options['parse'] === true
             ) {
-                $permissions[] = $name . '.' . $options['function'] . '.' . 'parse';
                 if($permission->get('name') === $name . '.' . $options['function'] . '.' . 'parse'){
                     $is_permission_parse = true;
                 }
