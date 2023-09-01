996529d49438eb5f5cd491933529f160516d49e9
remco van der Velde <remco.pc@outlook.com>
2023-09-01 14:42:35 +0200

    parse

--- src/Trait/Data/Read.php
+++ src/Trait/Data/Read.php
@@ -35,7 +35,7 @@ Trait Read {
             ],
             'function' => __FUNCTION__,
             'multiple' => true,
-            'relation' => $options['relation'] ?? false,
+            'relation' => $options['relation'] ?? true,
             'parse' => $options['parse'] ?? false,
         ];
         $ramdisk_record = $object->config('package.r3m_io/node.ramdisk');
