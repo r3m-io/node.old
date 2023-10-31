5b910c8febc8f21c01dd0bbc3b85eb387fe6aea4
remco van der Velde <remco.pc@outlook.com>
2023-10-31 07:24:25 +0100

    security debug

---
+++ src/Controller/Node.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace R3m\Io\Node\Controller;
+
+use Host\Api\Workandtravel\World\Service\Entity;
+use R3m\Io\App;
+
+
+use R3m\Io\Module\Controller;
+use R3m\Io\Module\Core;
+use R3m\Io\Module\Dir;
+use R3m\Io\Module\Response;
+use R3m\Io\Node\Model\Node as Model;
+
+use Exception;
+
+use R3m\Io\Exception\ObjectException;
+use R3m\Io\Exception\FileWriteException;
+
+class Node extends Controller {
+    const DIR = __DIR__ . '/';
+
+    public static function object_tree(App $object){
+        $dir = new Dir();
+        $read = $dir->read(
+            $object->config('project.dir.data') .
+            'Node' .
+            $object->config('ds') .
+            'Object' .
+            $object->config('ds'),
+            false
+        );
+        $data = [];
+        $data['nodeList'] = [];
+        $data['nodeList']['tree'] = $read;
+        return new Response(
+            $data,
+            Response::TYPE_JSON
+        );return new Response(
+            $data,
+            Response::TYPE_JSON
+        );
+    }
+
+}
