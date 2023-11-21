59512df3e4971d32cd692efcc5fbb1fb14bf6e3b
remco van der Velde <remco.pc@outlook.com>
2023-11-21 19:17:34 +0100

    is unique error

--- src/Validator/Is.Unique.php
+++ src/Validator/Is.Unique.php
@@ -54,6 +54,8 @@ function validate_is_unique(App $object, $value='', $attribute='', $validate='')
             }
         }
     }
+    d($attribute);
+    ddd($value);
     if (
         is_array($attribute) &&
         is_array($value)
