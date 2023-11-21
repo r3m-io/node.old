b69bc4924dd8cdf4c580636ae85e435b0edf5712
remco van der Velde <remco.pc@outlook.com>
2023-11-21 22:25:28 +0100

    is unique error

--- src/Trait/Data/Patch.php
+++ src/Trait/Data/Patch.php
@@ -229,7 +229,16 @@ Trait Patch {
                 $node->remove_null();
             }
             else {
-                $node->set($attribute, $value);
+                if(
+                    is_string($value) &&
+                    substr($value, 0, 1) === '{' &&
+                    substr($value, -1, 1) === '}'
+                ){
+                    $node->set($attribute, Core::object_merge($node->get($attribute), Core::object($value, Core::OBJECT_OBJECT)));
+                    $node->remove_null();
+                } else {
+                    $node->set($attribute, $value);
+                }
             }
         }
         $response = [];
