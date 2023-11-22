3ca9de86b5471416bba3dabdf31fcf7f18ce7539
remco van der Velde <remco.pc@outlook.com>
2023-11-22 20:33:16 +0100

    add wildcard (*) and where and filter options to relations

--- src/Trait/Data/Record.php
+++ src/Trait/Data/Record.php
@@ -33,6 +33,7 @@ Trait Record {
         )){
             return null;
         }
+        d($options);
         $list = $this->list($class, $role, $options);
         if(
             is_array($list) &&
