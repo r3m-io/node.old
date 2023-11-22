8a09a59c366b5aaff618658ce0299f3844b66406
remco van der Velde <remco.pc@outlook.com>
2023-11-22 19:39:52 +0100

    add wildcard (*) and where and filter options to relations

--- src/Trait/BinaryTree.php
+++ src/Trait/BinaryTree.php
@@ -1425,9 +1425,9 @@ Trait BinaryTree {
                 }
             }
             $file->seek($seek);
-            echo 'Seek 1: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
+//            echo 'Seek 1: ' . $seek . ' options.index: ' . $options['index'] . PHP_EOL;
             $line = $file->current();
-            echo $line . PHP_EOL;
+//            echo $line . PHP_EOL;
             $options['counter']++;
             if($options['counter'] > 1024){
                 throw new Exception('Out of range');
