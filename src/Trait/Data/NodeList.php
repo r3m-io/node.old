76da165476c5994cdfda387295114966f1b0be6b
remco van der Velde <remco.pc@outlook.com>
2023-11-23 18:56:49 +0100

    ;

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -61,6 +61,7 @@ Trait NodeList {
         $mtime = false;
         $name = Controller::name($class);
         $options = Core::object($options, Core::OBJECT_ARRAY);
+        d($options);
         if(!array_key_exists('function', $options)){
             $options['function'] = __FUNCTION__;
         }
